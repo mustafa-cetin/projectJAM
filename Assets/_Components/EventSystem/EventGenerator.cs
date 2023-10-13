96ae056f6f1eab578f48b75233692bdc092eb4b4
Niyazi Emre Akbaş <emreakbas46@outlook.com>
2023-10-13 23:59:24 +0300

    Events Scene Added

---
+++ Assets/_Components/EventSystem/EventGenerator.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class EventGenerator : MonoBehaviour
+{
+
+    public Events[] EventsArray;
+
+    public TMP_Text mainText;
+
+    public TMP_Text button1Text;
+
+    public TMP_Text button2Text;
+
+    public void callRandomEvent(){
+        int randomEventNum = Random.Range(1,8);
+
+        Events currentEvents = EventsArray[randomEventNum];
+
+        mainText.text = currentEvents.Event;
+
+        button1Text.text = currentEvents.EventChoices[0];
+
+        button2Text.text = currentEvents.EventChoices[1];
+
+    }
+
+    void Start(){
+        callRandomEvent();
+    }
+    
+    
+
+
+
+
+}
---
+++ Assets/_Components/EventSystem/Events.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Events/Events")]
+public class Events : ScriptableObject
+{
+    public string EventTitle;
+
+    public string Event;
+
+    public string[] EventChoices = new string[2];
+
+    /*
+    // Event 1: Resource Scarcity
+    string Event1 = "Your city faces a severe shortage of a crucial resource (e.g., food, fuel, or medicine). Citizens are getting desperate.";
+    string[] Event1Choices = {
+        "Implement rationing",
+        "Send scouts to search for resources",
+    };
+
+    // Event 2: Outsider Arrival
+    string Event2 = "A group of outsiders arrives at your city, seeking shelter and assistance.";
+    string[] Event2Choices = {
+        "Welcome them with open arms",
+        "Turn them away"
+    };
+
+    // Event 3: Epidemic Outbreak
+    string Event3 = "A deadly disease begins to spread among your citizens.";
+    string[] Event3Choices = {
+        "Isolate the sick",
+        "Conduct medical research to find a cure"
+    };
+
+    // Event 4: Discontent Rebellion
+    string Event4 = "Discontent among your citizens reaches a critical point, and they're threatening to rebel.";
+    string[] Event4Choices = {
+        "Suppress the rebellion with force",
+        "Negotiate with the leaders",
+    };
+
+    // Event 5: Resource Discovery
+    string Event5 = "Scouts discover a hidden cache of valuable resources in the wilderness.";
+    string[] Event5Choices = {
+        "Extract the resources immediately",
+        "Set up a more efficient operation",
+    };
+
+    // Event 6: Moral Dilemma
+    string Event6 = "A desperate mother pleads with you to allow her child to work in dangerous conditions to help her family.";
+    string[] Event6Choices = {
+        "Allow the child to work",
+        "Refuse and provide support to the family",
+    };
+
+    // Event 7: Natural Disaster
+    string Event7 = "The shelter flooded";
+    string[] Event7Choices = {
+        "Let the citizens drain the water",
+        "Strengthen the sewer system",
+    };
+
+    */
+}

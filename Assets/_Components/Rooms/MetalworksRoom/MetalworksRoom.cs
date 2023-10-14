d712e0b640c78cdb214c53d64411321e7ca9a6c7
hexapawa <115427747+mustafa-cetin@users.noreply.github.com>
2023-10-15 00:46:46 +0300

    uiresource metalworks

--- Assets/_Components/Rooms/MetalworksRoom/MetalworksRoom.cs
+++ Assets/_Components/Rooms/MetalworksRoom/MetalworksRoom.cs
@@ -14,12 +14,16 @@ public class MetalworksRoom : ResourceRoom
     }
     public override void Update() {
         base.Update();
+        
+        if (Worker!=null)
+        {
         if (Time.time>=betweenTimeCount+timer && Shelter.Instance.Electric>=5 && ready)
         {
             timer=Time.time;
             Shelter.Instance.ChangeMetal(5);
             
             Shelter.Instance.ChangeElectric(-1*5);
+        }
         }
     }
 
--- Assets/_Components/UI/UIResourceManager.cs
+++ Assets/_Components/UI/UIResourceManager.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class UIResourceManager : MonoBehaviour
-{
+{   
+    public float interval=20f;
     public TMP_Text foodText;
     public TMP_Text electricText;
     public TMP_Text metalText;
@@ -19,45 +20,52 @@ public class UIResourceManager : MonoBehaviour
                 public List<TMP_Text> metalNotifyTexts;
 
     public Slider rebelSlider;
+    private void Start() {
+        electricNotifyTexts=new List<TMP_Text>();
+        foodNotifyTexts=new List<TMP_Text>();
+        oxygenNotifyTexts=new List<TMP_Text>();
+        metalNotifyTexts=new List<TMP_Text>();
+    }
     private void Update() {
         foodText.text=Shelter.Instance.Food.ToString();
         electricText.text=Shelter.Instance.Electric.ToString();
         metalText.text=Shelter.Instance.Metal.ToString();
         oxygenText.text=Shelter.Instance.Oxygen.ToString();
         rebelSlider.value=Shelter.Instance.rebel;
+
     }
-    public void NotifyElectric(int value){
+    public void NotifyElectric(int value){electricNotifyTexts.RemoveAll(s => s == null);
         TMP_Text cloneNotify=Instantiate(notifyText,transform);
         electricNotifyTexts.Add(cloneNotify);
         cloneNotify.text=value.ToString();
         Vector3 position=electricText.transform.position;
-        position.y-=15f*electricNotifyTexts.Count;
+        position.y-=interval*electricNotifyTexts.Count;
         cloneNotify.transform.position=position;
     }
-    public void NotifyFood(int value){
+    public void NotifyFood(int value){foodNotifyTexts.RemoveAll(s => s == null);
         TMP_Text cloneNotify=Instantiate(notifyText,transform);
         foodNotifyTexts.Add(cloneNotify);
         cloneNotify.text=value.ToString();
         Vector3 position=foodText.transform.position;
-        position.y-=15f*foodNotifyTexts.Count;
+        position.y-=interval*foodNotifyTexts.Count;
         cloneNotify.transform.position=position;
     }
     public void NotifyOxygen(int value){
-        
+        oxygenNotifyTexts.RemoveAll(s => s == null);
         TMP_Text cloneNotify=Instantiate(notifyText,transform);
         oxygenNotifyTexts.Add(cloneNotify);
         cloneNotify.text=value.ToString();
         Vector3 position=oxygenText.transform.position;
-        position.y-=15f*oxygenNotifyTexts.Count;
+        position.y-=interval*oxygenNotifyTexts.Count;
         cloneNotify.transform.position=position;
     }
     public void NotifyMetal(int value){
-    
+        metalNotifyTexts.RemoveAll(s => s == null);
         TMP_Text cloneNotify=Instantiate(notifyText,transform);
         metalNotifyTexts.Add(cloneNotify);
         cloneNotify.text=value.ToString();
         Vector3 position=metalText.transform.position;
-        position.y-=15f*metalNotifyTexts.Count;
+        position.y-=interval*metalNotifyTexts.Count;
         cloneNotify.transform.position=position;
     }
 }

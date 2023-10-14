9e22a3638b79797d6b6105ac5ad7ee63e2affd97
Niyazi Emre Akbaş <emreakbas46@outlook.com>
2023-10-14 13:40:21 +0300

    character added

--- Assets/_Components/CitizenSystem/Citizen.cs
+++ Assets/_Components/CitizenSystem/Citizen.cs
@@ -19,7 +19,25 @@ public class Citizen : MonoBehaviour
     public int cooking; 
 
 
-    
+    public float moveSpeed = 5f; // Hareket hızı
+
+    void Update()
+    {
+        float moveInput = Input.GetAxis("Horizontal"); // Sağ veya sol ok tuşlarına basılınca -1, 0 veya 1 döndürür.
+
+        Vector3 newPosition = transform.position + new Vector3(moveInput * moveSpeed * Time.deltaTime, 0, 0);
+
+        if (moveInput < 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1); // Karakteri sağa döndür
+        }
+        else if (moveInput > 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1); // Karakteri sola döndür
+        }
+
+        transform.position = newPosition;
+    }
 
 
    

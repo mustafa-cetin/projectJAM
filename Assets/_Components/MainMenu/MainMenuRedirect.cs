d7a34a7a528dec9bc2c1c2db09aeaedaf54e4b90
Mustafa Cetin <115427747+mustafa-cetin@users.noreply.github.com>
2023-12-22 01:26:43 +0300

    added main menu to version text

--- Assets/_Components/MainMenu/MainMenuRedirect.cs
+++ Assets/_Components/MainMenu/MainMenuRedirect.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuRedirect : MonoBehaviour
 {
+   [SerializeField]
+   private TMP_Text versionText;
+   private void Start() {
+      versionText.text="Version: "+Application.version;
+   }
    public void GoGameScene(){
     SceneManager.LoadScene(1);
    }
